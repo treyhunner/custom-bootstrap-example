1df3ae75780f0c9633907ef377443881b1fd1b6a
Trey Hunner <trey@treyhunner.com>
2015-02-23 10:06:54 -0800

    Add bootstrap-sass example via bower/gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglifyjs');
+
+var config = {
+  bowerDir: './bower_components',
+  publicDir: './public',
+};
+
+gulp.task('fonts', function() {
+  return gulp.src([
+    config.bowerDir + '/bootstrap-sass-official/assets/fonts/**/*',
+  ])
+  .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('js', function() {
+  return gulp.src([
+    config.bowerDir + '/jquery/dist/jquery.min.js',
+    config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+  ])
+  .pipe(uglify('app.js', {
+    compress: false,
+    outSourceMap: true,
+  }))
+  .pipe(gulp.dest(config.publicDir + '/js'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('css/app.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    style: 'compressed',
+    includePaths: [config.bowerDir + '/bootstrap-sass-official/assets/stylesheets'],
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.publicDir + '/css'));
+});
+
+gulp.task('default', ['css', 'js', 'fonts']);
